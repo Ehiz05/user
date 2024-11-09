93d75ea9f1d8f41e68cfb340a9a7b5ee8f89eb56
Ehiz05 <sundayowonka@gmail.com>
2024-11-08 22:46:34 -0800

    Week 2 Python Assignment

---
+++ Documents/PLP_python/wktwo.py
@@ -0,0 +1,26 @@
+# Question 1
+my_list = []
+
+# Question 2
+my_list = [10, 20, 30, 40]
+
+# Question 3
+my_list.insert(1, 15)
+
+# Question 4
+my_list2 = [50, 60, 70]
+
+my_list.extend(my_list2)
+
+# Question 5
+my_list.remove(70)
+
+# Question 6
+asc_list = sorted(my_list)
+
+print(asc_list)
+
+# Question 7
+my_list[2]
+
+print(my_list[2])
